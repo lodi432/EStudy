3e7bfc4871cfc91827e6cb9946ba712ec545ebb0
lodi432 <glavacevic.d@gmail.com>
2017-12-10 22:26:20 +0100

    First Git Bash Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Trgovina Shop</title>
+ <link rel="stylesheet" href ="css/bootstrap.min.css">
+ <link rel="stylesheet" href="/css/main.css">
+ <meta name="viewport" content="width = device -width,initial-scale=1,user-scalable=no">
+ <!--<script src="/css/jquery-3.2.1.min.js"></script>-->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <a href="index.php" class="navbar-brand">Trgovina Shop</a>
+      <ul class ="nav navbar-nav">
+         <li class="dropdown">
+           <a href="#" class="dropdown-toggle" data-toggle="dropdown"></a>
+         </li>
+      </ul>
+     </div>
+   </nav>
+</body>
+</html>
