29b2543fd9553634499f3a80572a2af1399cdd41
lodi432 <glavacevic.d@gmail.com>
2017-12-10 22:51:29 +0100

    Dropdown menu bar items, and Javascript cdn version fix

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
+
+<!DOCTYPE html>
+   <html lang="en">
+     <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1", user-scaleable=no>
 <html>
 <head>
- <title>Trgovina Shop</title>
- <link rel="stylesheet" href ="css/bootstrap.min.css">
- <link rel="stylesheet" href="/css/main.css">
- <meta name="viewport" content="width = device -width,initial-scale=1,user-scalable=no">
- <!--<script src="/css/jquery-3.2.1.min.js"></script>-->
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
- <script src="js/bootstrap.min.js"></script>
+  <!-- jQuery (potrebno za Bootstrap's JavaScript pluginove) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <title>Trgovina Shop</title>
+  <!-- Bootstrap -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/mainx.css" />
 </head>
 <body>
   <nav class="navbar navbar-default navbar-fixed-top">
@@ -15,7 +23,13 @@
       <a href="index.php" class="navbar-brand">Trgovina Shop</a>
       <ul class ="nav navbar-nav">
          <li class="dropdown">
-           <a href="#" class="dropdown-toggle" data-toggle="dropdown"></a>
+           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+           <ul class="dropdown-menu role" role="menu">
+             <li><a href="#">Shirts </a></li>
+             <li><a href="#">Pants </a></li>
+             <li><a href="#">Shoes </a></li>
+             <li><a href="#">Accessories </a></li>
+           </ul>
          </li>
       </ul>
      </div>
